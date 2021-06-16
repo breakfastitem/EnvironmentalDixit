b047ec37b53111a213b4bb7c6f183830b023829e
Kyle Worcester-Moore <25kylworc@gmail.com>
2021-06-16 12:40:13 -0700

    add some helpful getting started comments

--- server.js
+++ server.js
@@ -6,13 +6,32 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const mongoose = require("mongoose");
 
-// For running in production:
-mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.jfqke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+if (process.env.NODE_ENV === "production") {
+
+    // ---- For running in production: -----
+    mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.jfqke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
     , { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-// For local testing, run the comands: mkdir data; mkdir data/db; mongod --noauth --dbpath ./data/db
-// mongoose.connect('mongodb://localhost/test?retryWrites=true&w=majority'
-//     , { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+} else if (process.env.NODE_ENV === 'development') {
+
+    /* ---- For local testing: -----
+    1. run the comands:
+       mkdir data
+       mkdir data/db
+       mongod --noauth --dbpath ./data/db
+    /  (note: on windows I think you use reverse slashes for the ./data/db path)
+    2. create a text file in the same folder as server.js and call it ".env" (with the . at the front and no file extension).
+        In that file add theses lines (to set the environment variables):
+           NODE_ENV=development
+           API_KEY=replace_this_with_our_flicker_api_key
+    3. then run this script with node in a new terminal/prompt (ie run: "node server.js")
+    */
+    mongoose.connect('mongodb://localhost/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+} else if (process.env.NODE_ENV === undefined) {
+    console.log("Uh oh! The NODE_ENV environment variable isn't set. Please set it to 'development' for testing or 'production' for the main server. (see the \"For local testing\" comment in server.js for details)")
+}
 
 //models
 const db = require("./models");
